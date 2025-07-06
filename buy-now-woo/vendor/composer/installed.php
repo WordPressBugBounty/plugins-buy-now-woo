ff672edcf412600978fff8886f0f4cf7be5bffb9
Sean Murphy <sean@iamseanmurphy.com>
2025-07-06 16:19:03 +0000

    1.1.0

---
+++ buy-now-woo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeixer/buy-now-woo',
+        'pretty_version' => '1.1.x-dev',
+        'version' => '1.1.9999999.9999999-dev',
+        'reference' => '2a8eadde665339cde615f9816f9ce64cbf5aa051',
+        'type' => 'woocommerce-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codeixer/buy-now-woo' => array(
+            'pretty_version' => '1.1.x-dev',
+            'version' => '1.1.9999999.9999999-dev',
+            'reference' => '2a8eadde665339cde615f9816f9ce64cbf5aa051',
+            'type' => 'woocommerce-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
